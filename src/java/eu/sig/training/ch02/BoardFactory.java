ac8999fb35aa35452b46ad7a0a16d7c818b3a5a2
saunte <ks@saunte.com>
2020-01-29 11:24:13 +0100

    test1

--- src/java/eu/sig/training/ch02/BoardFactory.java
+++ src/java/eu/sig/training/ch02/BoardFactory.java
@@ -1,5 +1,7 @@
 package eu.sig.training.ch02;
 
+
+
 public class BoardFactory {
     // tag::createBoard[]
     public Board createBoard(Square[][] grid) {
@@ -25,6 +27,35 @@ public class BoardFactory {
     }
     // end::createBoard[]
 }
+
+class BoardCreator {
+    private Square[][] grid;
+    private Board board;
+    private int width;
+    private int height;
+
+    BoardCreator(Square[][] grid) {
+        assert grid != null;
+        this.grid = grid;
+        this.board = new Board(grid);
+        this.width = board.getWidth();
+        this.height = board.getHeight();
+    }
+
+    Board create() {
+        for (int x=0;x<width;x++)
+          for (int y=0;x<height;y++) {
+            Square square = grid[x][y];
+            for (Direction dir : Direction.values())
+                setLink(square, dir, x, y);
+          }
+        return this.board;
+    }
+
+    private void setLink(Square square, Direction dir, int x, int y) {
+        
+    }
+} 
 
 class Board {
     @SuppressWarnings("unused")
